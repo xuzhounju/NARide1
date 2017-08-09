9f1b97aadb96bdbf36df2266f944d537182a581c
TianyiM <matianyi1029@gmail.com>
2017-08-09 18:29:20 -0400

    change quest

--- pages/quest/quest.js
+++ pages/quest/quest.js
@@ -1,9 +1,6 @@
 //request.js
 var util = require('../../utils/util.js')
 
-function initSubMenuDisplay() {
-  return ['hidden', 'hidden'];
-}
 var app = getApp()
 Page({
   data: {
@@ -11,64 +8,25 @@ Page({
     nowDate: '',
     endDate: '',
     placeArray: ['Amherst', 'BDL Airport', 'Boston', 'Logan Airport', 'NYC'],
-
     userInfo: {},
     departure: 0,
     arrival: 1,
     eDate: '',
     eTime: '',
     lDate: '',
-    lTime: '',
-    pNumber: 1,
-    memo: ''
+    lTime: ''
   },
   onLoad: function () {
     console.log('onLoad')
-    // var d = Date.now()
-    // var year = d.getFullYear()
-    // var month = d.getMonth()
-    // var day = d.getDate()
-    // this.setDate({
-    //   nowDate: year+'-'+month+'-'+day,
-    //   endDate: (year+1)+'-'+(month+1)%12+day
-    // })
-
     var that = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function (userInfo) {
       //更新数据
       that.setData({
         userInfo: userInfo
-
       })
     })
-
-
-    // wx.connectSocket({
-    //   url: 'wss://kunwang.us'
-    // })
-
-    // wx.onSocketOpen(function (res) {
-    //   console.log('WebSocket连接已打开！')
-    // })
-
-    // wx.onSocketError(function (res) {
-    //   console.log('WebSocket连接打开失败，请检查！')
-    // })
-
-
-
-
   },
-
-
-  bindPNumberPickerChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    this.setData({
-      pNumber: e.detail.value
-    })
-  },
-
 
 
   bindDeparturePickerChange: function (e) {
@@ -112,7 +70,6 @@ Page({
   },
 
   formSubmit: function (e) {
-    console.log(e)
     if (e.detail.value.eDate <= e.detail.value.lDate) {
       var earlist = new Date(e.detail.value.eDate + ' ' + e.detail.value.eTime)
       console.log('E:', earlist)
@@ -125,48 +82,20 @@ Page({
       mydata.departure = parseInt(mydata.departure) + 1;
       mydata.arrival = parseInt(mydata.arrival) + 1;
       wx.request({
-        url: 'https://kunwang.us/new/', //仅为示例，并非真实的接口地址
-
-
-        data: mydata,
-
-
-        method: "POST",
-
+        url: 'https://kunwang.us/list/' + mydata.earliest + '/' + mydata.latest + '/' + mydata.departure + '/' + mydata.arrival + '/', //仅为示例，并非真实的接口地址 //仅为示例，并非真实的接口地址
         header: {
           'content-type': 'application/x-www-form-urlencoded'
         },
         success: function (res) {
-          console.log(res.data)
+          app.searchResult = res.data;
+          wx.navigateTo({
+            url: '../questResult/questResult',
+          })
         }
       })
-      console.log('form发生了submit事件，携带数据为：', e.detail.value)
-    } else {
-      console.log('最晚日期早于最早日期 ')
-    }
+    } 
   },
-  formReset: function () {
-    this.setData({
-      departure: 0,
-      arrival: 1,
-      eDate: '',
-      eTime: '',
-      lDate: '',
-      lTime: '',
-      pNumber: 1,
-      memo: ''
-    })
-    console.log('form发生了reset事件')
-  },
-
   onShareAppMessage: function () {
 
   }
-
-
-
-
-
-
-
 })
