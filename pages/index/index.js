0e6aef365c3e74e47ef576b540e01748ede1d46b
TianyiM <matianyi1029@gmail.com>
2017-08-18 15:47:43 -0400

    index 1.0

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,7 +8,10 @@ Page({
   data: {
     navTab: ["全部", "我是司机", "我是乘客"],
     currentNavtab: "0",
-    userInfo:''
+    userInfo:'',
+    sResult:null,
+    purpose: ['招客', '搭车'],
+    purposeID: 1
   },
 
   /**
@@ -18,8 +21,39 @@ Page({
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
-    this.refresh();
-
+    var nowTime = new Date();
+    var latestTime = new Date(nowTime.getTime() + 2 * 30 * 24 * 3600 * 1000);
+    nowTime = Math.floor(nowTime.getTime() / 1000.0);
+    latestTime = Math.floor(latestTime.getTime() / 1000.0);
+    var departure = 0;
+    var arrival = 0;
+    wx.request({
+      url: 'https://kunwang.us/list/' + nowTime + '/' + latestTime + '/' + departure + '/' + arrival + '/', //仅为示例，并非真实的接口地址 //仅为示例，并非真实的接口地址
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        var result = res.data;
+      }
+    })
+    var sResult = [];
+    var placeArray = app.globalData.place;
+      for (var i = 0; i < result.length; i++) {
+        if (result[i]) {
+          result[i].fields.departure = placeArray[result[i].fields.departure - 1];
+          result[i].fields.arrival = placeArray[result[i].fields.arrival - 1];
+          result[i].fields.earliest = new Date(result[i].fields.earliest);
+          result[i].fields.earliest = result[i].fields.earliest.toLocaleString();
+          result[i].fields.latest = new Date(result[i].fields.latest);
+          result[i].fields.latest = result[i].fields.latest.toLocaleString();
+          sResult.push(result[i].fields);
+        }
+      }
+    sResult = sResult.reverse();
+    app.globalData.sResult = sResult;
+    this.setData({
+      sResult: sResult
+    })
 
     wx.login({
       success: function (res) {
@@ -58,6 +92,8 @@ Page({
     this.setData({
       currentNavtab: e.currentTarget.dataset.idx
     });
+    console.log(e.currentTarget.dataset.idx)
   },
+
 })
 
